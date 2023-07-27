669a0a2bb054914ea1413fea5695eac653320bd3
ssuisig <jsy6430000@naver.com>
2023-07-27 17:56:03 +0900

    diary_update

--- app/src/main/java/ddwu/com/mobile/anylearn/MyDiaryMain.kt
+++ app/src/main/java/ddwu/com/mobile/anylearn/MyDiaryMain.kt
@@ -8,18 +8,12 @@ class MyDiaryMain : AppCompatActivity() {
 
     lateinit var mdmBinding: ActivityMyDiaryMainBinding
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        mdmBinding = MyDiaryMainBinding.inflate(layoutInflater)
-//        setContentView(mdmBinding.root)
-//
-//        val calendarView = mdmBinding.materialCalendar
-//
-//        calendarView.setSelectedDate(Calendar.getInstance())
-//
-//        calendarView.setOnDateChangedListener { widget, date, selected ->
-//            // 날짜를 선택했을 때 발생하는 이벤트를 처리하는 코드를 추가
-//
-//        }
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mdmBinding = ActivityMyDiaryMainBinding.inflate(layoutInflater)
+        setContentView(mdmBinding.root)
+    }
 }
+
+
+
