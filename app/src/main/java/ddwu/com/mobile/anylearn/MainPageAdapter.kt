68483eec1bbf1f0498b749ab4eb8acb6837ac25f
dahye <color4541@naver.com>
2023-07-27 16:37:22 +0900

    anylearn beta1

--- app/src/main/java/ddwu/com/mobile/anylearn/MainPageAdapter.kt
+++ app/src/main/java/ddwu/com/mobile/anylearn/MainPageAdapter.kt
@@ -34,55 +34,52 @@ class MainPageAdapter (private val menuItems: List<MenuItem>, private val contex
 
         if (currentItem.button1Text != " ") {
             holder.button1.text = currentItem.button1Text
-            // Set onClickListener for button1 if needed
-            // holder.button1.setOnClickListener { /* Your implementation here */ }
-        } else {
-            holder.button1.visibility = View.GONE
-            holder.button1.isEnabled = false
-        }
+            holder.button1.setOnClickListener{
+                when(currentItem.button1Text){
+                    "단계 설정 ①", "단계 설정 ②", "단계 설정 ③"-> {
+                        val alertDialogBuilder = AlertDialog.Builder(context)
+                        alertDialogBuilder.setTitle("단계 선택")
 
-        holder.button1.setOnClickListener{
-            when(currentItem.button1Text){
-                "단계 설정 ①", "단계 설정 ②", "단계 설정 ③"-> {
-                    val alertDialogBuilder = AlertDialog.Builder(context)
-                    alertDialogBuilder.setTitle("단계 선택")
+                        val items = arrayOf("①", "②", "③")
+                        var selectedStep = -1
 
-                    val items = arrayOf("①", "②", "③")
-                    var selectedStep = -1
+                        alertDialogBuilder.setSingleChoiceItems(items, -1) { dialog, which ->
+                            selectedStep = which
+                        }
 
-                    alertDialogBuilder.setSingleChoiceItems(items, -1) { dialog, which ->
-                        selectedStep = which
-                    }
-
-                    alertDialogBuilder.setPositiveButton("확인") { dialog, which ->
-                        // 팝업 확인 버튼을 눌렀을 때 선택한 단계에 대한 처리를 수행합니다.
-                        if (selectedStep != -1) {
-                            // 선택한 단계를 사용하여 처리할 작업을 수행합니다.
-                            when (selectedStep) {
-                                0 -> {
-                                    // 단계 1 선택 시 처리
-                                    holder.button1.text = "단계 설정 ①"
-                                }
-                                1 -> {
-                                    // 단계 2 선택 시 처리
-                                    holder.button1.text = "단계 설정 ②"
-                                }
-                                2 -> {
-                                    // 단계 3 선택 시 처리
-                                    holder.button1.text = "단계 설정 ③"
+                        alertDialogBuilder.setPositiveButton("확인") { dialog, which ->
+                            // 팝업 확인 버튼을 눌렀을 때 선택한 단계에 대한 처리를 수행합니다.
+                            if (selectedStep != -1) {
+                                // 선택한 단계를 사용하여 처리할 작업을 수행합니다.
+                                when (selectedStep) {
+                                    0 -> {
+                                        // 단계 1 선택 시 처리
+                                        holder.button1.text = "단계 설정 ①"
+                                    }
+                                    1 -> {
+                                        // 단계 2 선택 시 처리
+                                        holder.button1.text = "단계 설정 ②"
+                                    }
+                                    2 -> {
+                                        // 단계 3 선택 시 처리
+                                        holder.button1.text = "단계 설정 ③"
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    alertDialogBuilder.setNegativeButton("취소") { dialog, which ->
-                        // 팝업 취소 버튼을 눌렀을 때 수행할 동작을 작성합니다.
-                    }
+                        alertDialogBuilder.setNegativeButton("취소") { dialog, which ->
+                            // 팝업 취소 버튼을 눌렀을 때 수행할 동작을 작성합니다.
+                        }
 
-                    val alertDialog = alertDialogBuilder.create()
-                    alertDialog.show()
+                        val alertDialog = alertDialogBuilder.create()
+                        alertDialog.show()
+                    }
                 }
             }
+        } else {
+            holder.button1.visibility = View.GONE
+            holder.button1.isEnabled = false
         }
 
         holder.button2.text = currentItem.button2Text
