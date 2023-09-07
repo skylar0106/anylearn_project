f7304fd26a83e201fa3c58d3107513ef22aefd63
jsy <jsy6430000@naver.com>
2023-09-07 18:44:05 +0900

    birthCalendar

--- app/src/main/java/ddwu/com/mobile/anylearn/SignUp1.kt
+++ app/src/main/java/ddwu/com/mobile/anylearn/SignUp1.kt
@@ -1,9 +1,11 @@
 package ddwu.com.mobile.anylearn
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ddwu.com.mobile.anylearn.R
 import ddwu.com.mobile.anylearn.databinding.ActivitySignUp1Binding
+import java.util.*
 
 class SignUp1 : AppCompatActivity() {
 
@@ -17,9 +19,22 @@ class SignUp1 : AppCompatActivity() {
         val email = suBinding1.editEmail.text.toString()
         val name = suBinding1.editName.text.toString()
         val birth = suBinding1.editBirth.text.toString()
+
+
+        suBinding1.btnBirth.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val data = DatePickerDialog.OnDateSetListener { view, year, month, day ->
+                val selectedDate = "${year}/${month}/${day}"
+                suBinding1.editBirth.setText(selectedDate)
+            }
+            DatePickerDialog(this, data, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
         val phone = suBinding1.editPhone.text.toString()
         val pwd = suBinding1.editPwd2.text.toString()
         val pwdCheck = suBinding1.editPwdCheck.text.toString()
+
+
 
 
     }
