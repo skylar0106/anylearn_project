ed66d425c36334b7dfd97347b56044d0eb922b26
dahye <color4541@naver.com>
2023-09-11 21:04:13 +0900

    헤더 response

--- app/src/main/java/ddwu/com/mobile/anylearn/HeaderInterceptor.kt
+++ app/src/main/java/ddwu/com/mobile/anylearn/HeaderInterceptor.kt
@@ -1,10 +1,8 @@
 import ddwu.com.mobile.anylearn.MySharedPreferences
 import okhttp3.Interceptor
-import okhttp3.Request
 import okhttp3.Response
-import java.io.IOException
 
-class CsrfTokenInterceptor(private val mySharedPreferences: MySharedPreferences) : Interceptor {
+class HeaderInterceptor(private val mySharedPreferences: MySharedPreferences) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val response = chain.proceed(request)
